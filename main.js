09dda2b769468439c21a420099553dcfb165df10
brittocx <brittocx@gmail.com>
2022-06-01 16:05:18 -0300

    added about and services sections, and some responsive menu

---
+++ main.js
@@ -0,0 +1,33 @@
+/* =================== TOGGLE HAMBURGUER MENU =================== */
+
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+}
+
+/* =================== HIDE MENU AFTER LINK CLICK =================== */
+
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
+}
+
+/* =================== CHANGE HEADER WHEN SCROLL =================== */
+
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
